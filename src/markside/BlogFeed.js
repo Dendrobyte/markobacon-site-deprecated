9640f796d71b199849539470c0f3697369806e82
Dendrobyte <markbacon78@gmail.com>
2021-08-16 15:41:04 -0400

    Talking to the back-end (reformatted)

--- src/markside/BlogFeed.js
+++ src/markside/BlogFeed.js
@@ -1,11 +1,21 @@
 import BlogPost from "./BlogPost";
-
-// TODO: Make the feed load 10 at first and then be dynamic. Loading the feed is where things are pulled/rendered from DB...?
+import {useState, useEffect} from 'react';
+require('dotenv').config({ silent: true })
 
 function BlogFeed(props){
 
+    const [test, setTest] = useState(null);
+
+    // TODO: Make the feed load 10 at first and then be dynamic. Loading the feed is where things are pulled/rendered from DB...?
+    useEffect(() =>{
+        fetch(`http://localhost:3001/allentries`,)
+            .then((res) => res.json())
+            .then((data) => setTest(data.message));
+    },  []);
+
     return (
         <>
+            <p>{test}</p>
             <BlogPost />
             <BlogPost />
             <BlogPost />
